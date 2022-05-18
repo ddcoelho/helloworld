858e709b6ef77d58c56e2e3255838ac97df6e2e4
Daniel Coelho <coelhodesigns@gmail.com>
2022-05-18 21:14:59 +0100

    Hello world program

---
+++ main.go
@@ -0,0 +1,15 @@
+// What does package main mean
+////
+package main
+
+// What does import "fmt" mean
+////
+import "fmt"
+
+// What does func main() mean
+func main() {
+	fmt.Print("Hello vorld")
+}
+
+// How does the program run
+// how is the code organized
